576708267a7b5bbee781eb92a1b9698ea4a42c95
Vu Nguyen <vu.nguyen@treehouse.finance>
2022-06-04 15:15:49 +0700

    Make contrast better statusline

--- lua/lualine/themes/vn-night.lua
+++ lua/lualine/themes/vn-night.lua
@@ -3,37 +3,37 @@ local schema = require('vn-night.schema')
 local line_theme = {}
 
 line_theme.normal = {
-	a = { bg = schema.purple, fg = schema.fg, gui = "bold" },
+	a = { bg = schema.purple, fg = schema.gray[1], gui = "bold" },
 	b = { bg = schema.gray[4], fg = schema.gray[8] },
 	c = { bg = schema.gray[4], fg = schema.fg },
 }
 
 line_theme.insert = {
-	a = { bg = schema.teal, fg = schema.fg, gui = "bold" },
+	a = { bg = schema.teal, fg = schema.gray[1], gui = "bold" },
 	b = { bg = schema.gray[4], fg = schema.gray[8] },
 	c = { bg = schema.gray[4], fg = schema.fg },
 }
 
 line_theme.command = {
-	a = { bg = schema.orange, fg = schema.fg, gui = "bold" },
+	a = { bg = schema.orange, fg = schema.gray[1], gui = "bold" },
 	b = { bg = schema.gray[4], fg = schema.gray[8] },
 	c = { bg = schema.gray[4], fg = schema.fg },
 }
 
 line_theme.visual = {
-	a = { bg = schema.blue, fg = schema.fg, gui = "bold" },
+	a = { bg = schema.blue, fg = schema.gray[1], gui = "bold" },
 	b = { bg = schema.gray[4], fg = schema.gray[8] },
 	c = { bg = schema.gray[4], fg = schema.fg },
 }
 
 line_theme.replace = {
-	a = { bg = schema.red, fg = schema.fg, gui = "bold" },
+	a = { bg = schema.red, fg = schema.gray[1], gui = "bold" },
 	b = { bg = schema.gray[4], fg = schema.gray[8] },
 	c = { bg = schema.gray[4], fg = schema.fg },
 }
 
 line_theme.inactive = {
-	a = { bg = schema.purple, fg = schema.fg, gui = "bold" },
+	a = { bg = schema.purple, fg = schema.gray[1], gui = "bold" },
 	b = { bg = schema.gray[3], fg = schema.gray[6] },
 	c = { bg = schema.gray[3], fg = schema.gray[6] },
 }
